0bfc07593fe6e1c95d16135fe83a2c5c9e3afa38
phvleite <phvleite@gmail.com>
2022-07-30 15:49:31 -0300

    [REQ-06] realizado

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,11 +1,50 @@
 import { Request, Response } from 'express';
 import ProductService from '../services/products.service';
 
+const messages = [
+  {
+    message: '"name" is required',
+    code: 400,
+  },
+  {
+    message: '"name" must be a string',
+    code: 422,
+  },
+  {
+    message: '"name" length must be at least 3 characters long',
+    code: 422,
+  },
+  {
+    message: '"amount" is required',
+    code: 400,
+  },
+  {
+    message: '"amount" must be a string',
+    code: 422,
+  },
+  {
+    message: '"amount" length must be at least 3 characters long',
+    code: 422,
+  },
+];
+
 class ProductController {
   constructor(private productService = new ProductService()) {}
 
   public create = async (req: Request, res: Response) => {
-    const product = req.body;
+    const { error, value } = this.productService.validateProduct(req.body);
+    if (error) {
+      const { message } = error;
+      let code = 0;
+      messages.forEach((mess) => {
+        if (mess.message === message) {
+          code = mess.code;
+        }
+      });
+      return res.status(code).json({ message });
+    }
+
+    const product = value;
 
     const productCreated = await this.productService.create(product);
     res.status(201).json(productCreated);
--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -1,3 +1,4 @@
+import Joi from 'joi';
 import connection from '../models/connection';
 import ProductModel from '../models/product.model';
 import Product from '../interfaces/product.interface';
@@ -17,6 +18,14 @@ class ProductService {
     const products = await this.model.list();
     return products;
   }
+
+  public validateProduct = (product: Product) => {
+    const schema = Joi.object().keys({
+      name: Joi.string().required().min(3),
+      amount: Joi.string().required().min(3),
+    });
+    return schema.validate(product);
+  };
 }
 
 export default ProductService;
